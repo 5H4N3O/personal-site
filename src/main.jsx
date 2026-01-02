531b1e661a3851bdf92cd184a8e9ed93997cae09
5H4N3O <shanepratt2002@gmail.com>
2026-01-02 14:19:20 -0600

    Troubleshooting 404 Errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-const savedPath = sessionStorage.getItem('spa-path');
-if (savedPath) {
-  sessionStorage.removeItem('spa-path');
-  window.history.replaceState(null, '', savedPath);
-}
-
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
